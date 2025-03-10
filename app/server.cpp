748947d202f2a126ba812401622146dc8eeb74c5
Shreyas Jenner <shreyasjenner4@gmail.com>
2025-03-10 14:30:45 +0530

    closes 7

--- app/server.cpp
+++ app/server.cpp
@@ -1,11 +1,11 @@
-#include "server/server.hpp"
+#include "node/node.hpp"
 
 #include <thread>
 
-void start_recv(Server *s, std::vector<Users> *ptr) {
+void start_recv(Node *n, int sock) {
   std::string msg;
   while (true) {
-    msg = s->recv_msg(ptr->begin()->listeningSocket);
+    msg = n->recv_msg(sock);
     std::cout << msg << '\n';
     if (msg == "STOP") {
       break;
@@ -13,11 +13,11 @@ void start_recv(Server *s, std::vector<Users> *ptr) {
   }
 }
 
-void start_send(Server *s, std::vector<Users> *ptr) {
+void start_send(Node *n, int sock) {
   std::string msg;
   while (true) {
     std::cin >> msg;
-    s->send_msg(ptr->begin()->sendingSocket, msg);
+    n->send_msg(sock, msg);
     if (msg == "STOP") {
       break;
     }
@@ -25,15 +25,17 @@ void start_send(Server *s, std::vector<Users> *ptr) {
 }
 
 int main() {
-  Server *s = new Server("localhost", "8080");
-  s->start_server();
-  s->accept_conns();
+  std::string ip, port;
+  int sock;
 
-  auto ptr = s->get_user_data();
+  // Create node and start listening as server
+  Node *n = new Node(SERVER, "localhost", "8080");
+  n->start_node();
+  sock = n->accept_conns();
 
   // NOTE: thread to recv messages
-  std::thread t1(start_recv, s, ptr);
-  std::thread t2(start_send, s, ptr);
+  std::thread t1(start_recv, n, sock);
+  std::thread t2(start_send, n, sock);
 
   t1.join();
   t2.join();
