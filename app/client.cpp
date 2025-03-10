3f20f44c960ab424205d56a8abe5644973a83b07
Shreyas Jenner <shreyasjenner4@gmail.com>
2025-03-10 09:19:52 +0530

    added temporary thread function to app files

--- app/client.cpp
+++ app/client.cpp
@@ -1,5 +1,29 @@
 #include "client/client.hpp"
 
+#include <thread>
+
+void start_recv(Client *c) {
+  std::string msg;
+  while (true) {
+    msg = c->recv_msg();
+    std::cout << msg << '\n';
+    if (msg == "STOP") {
+      break;
+    }
+  }
+}
+
+void start_send(Client *c) {
+  std::string msg;
+  while (true) {
+    std::cin >> msg;
+    c->send_msg(msg);
+    if (msg == "STOP") {
+      break;
+    }
+  }
+}
+
 int main() {
   Client *c = new Client("localhost", "1234");
 
@@ -7,9 +31,12 @@ int main() {
 
   c->connect_to("localhost", "8080");
 
-  c->send_msg("hello");
+  // NOTE: temp code to thread message handling
+  std::thread t1(start_recv, c);
+  std::thread t2(start_send, c);
 
-  std::cout << c->recv_msg() << '\n';
+  t1.join();
+  t2.join();
 
   return 0;
 }
--- app/server.cpp
+++ app/server.cpp
@@ -1,5 +1,29 @@
 #include "server/server.hpp"
 
+#include <thread>
+
+void start_recv(Server *s, std::vector<Users> *ptr) {
+  std::string msg;
+  while (true) {
+    msg = s->recv_msg(ptr->begin()->listeningSocket);
+    std::cout << msg << '\n';
+    if (msg == "STOP") {
+      break;
+    }
+  }
+}
+
+void start_send(Server *s, std::vector<Users> *ptr) {
+  std::string msg;
+  while (true) {
+    std::cin >> msg;
+    s->send_msg(ptr->begin()->sendingSocket, msg);
+    if (msg == "STOP") {
+      break;
+    }
+  }
+}
+
 int main() {
   Server *s = new Server("localhost", "8080");
   s->start_server();
@@ -7,9 +31,12 @@ int main() {
 
   auto ptr = s->get_user_data();
 
-  std::cout << s->recv_msg(ptr->begin()->listeningSocket) << '\n';
+  // NOTE: thread to recv messages
+  std::thread t1(start_recv, s, ptr);
+  std::thread t2(start_send, s, ptr);
 
-  s->send_msg(ptr->begin()->sendingSocket, "Hello from server");
+  t1.join();
+  t2.join();
 
   return 0;
 }
